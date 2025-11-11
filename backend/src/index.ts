d21f674742574960fc15e85da4c51ec5a37d950c
Nimesh1010 <69566587+Nimesh1010@users.noreply.github.com>
2025-11-11 10:08:30 +0530

    Login and Authentication Added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import reservationRoutes from "./routes/reservationRoutes.js";
+import authRoutes from "./routes/authRoutes.js"
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 
 // API routes
 app.use("/api/reservations", reservationRoutes);
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
